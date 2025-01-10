9f24948fc1eee6a4ce40c4fcd06471991f98aa67
Ahmad Naufal Labiib Nabhaan <ahmad.nabhaan@students.amikom.ac.id>
2025-01-10 20:38:50 +0700

    update neuos

--- neuos.py
+++ neuos.py
@@ -79,14 +79,16 @@ best_throughput = -float('inf')
 def select_dvfs(df_prof):
     baseline_dvfs = df_prof["cpu_cores"].min(), df_prof["cpu_freq"].min(), df_prof["gpu_freq"].min(), df_prof["memory_freq"].min(), df_prof["cl"].min()
     baseline = df_prof[(df_prof["cpu_cores"] == baseline_dvfs[0]) & (df_prof["cpu_freq"] == baseline_dvfs[1]) & (df_prof["gpu_freq"] == baseline_dvfs[2]) & (df_prof["memory_freq"] == baseline_dvfs[3]) & (df_prof["cl"] == baseline_dvfs[4])]
-    baseline_troughput, baseline_power = baseline["throughput"], baseline["power"]
+    baseline_troughput, baseline_power = int(baseline["throughput"].iloc[0]), int(baseline["power"].iloc[0])
     chosen_dvfs[0] = list(baseline_dvfs)
     moderate = df_prof[(df_prof["throughput"] >= (baseline_troughput * SpeedUp_PowerUp[1][0])) & (df_prof["power"] >= (baseline_power * SpeedUp_PowerUp[1][1]))]
-    moderate_dvfs = moderate["cpu_cores"].min(), moderate["cpu_freq"].min(), moderate["gpu_freq"].min(), moderate["memory_freq"].min(), moderate["cl"].min()
-    chosen_dvfs[1] = list(moderate_dvfs)
+    if moderate:
+        moderate_dvfs = moderate["cpu_cores"].min(), moderate["cpu_freq"].min(), moderate["gpu_freq"].min(), moderate["memory_freq"].min(), moderate["cl"].min()
+        chosen_dvfs[1] = list(moderate_dvfs)
     high = df_prof[(df_prof["throughput"] >= (baseline_troughput * SpeedUp_PowerUp[2][0])) & (df_prof["power"] >= (baseline_power * SpeedUp_PowerUp[2][1]))]
-    high_dvfs = high["cpu_cores"].min(), high["cpu_freq"].min(), high["gpu_freq"].min(), high["memory_freq"].min(), high["cl"].min()
-    chosen_dvfs[2] = list(high_dvfs)
+    if high:
+        high_dvfs = high["cpu_cores"].min(), high["cpu_freq"].min(), high["gpu_freq"].min(), high["memory_freq"].min(), high["cl"].min()
+        chosen_dvfs[2] = list(high_dvfs)
 
 def update_output(throughput, power, configs, df_prof):
     df_prof["throughput"][(df_prof["cpu_cores"] == configs[0]) & (df_prof["cpu_freq"] == configs[1]) & (df_prof["gpu_freq"] == configs[2]) & (df_prof["memory_freq"] == configs[3]) & (df_prof["cl"] == configs[4])] = throughput
