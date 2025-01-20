b84fb51752f7ae82b1d508844dc1e9763c87e69a
Ahmad Naufal Labiib Nabhaan <ahmad.nabhaan@students.amikom.ac.id>
2025-01-20 08:16:54 +0700

    update dvfs setting

--- neuos.py
+++ neuos.py
@@ -91,11 +91,11 @@ def select_dvfs(df_prof):
     baseline = df_prof[(df_prof["cpu_cores"] == baseline_dvfs[0]) & (df_prof["cpu_freq"] == baseline_dvfs[1]) & (df_prof["gpu_freq"] == baseline_dvfs[2]) & (df_prof["memory_freq"] == baseline_dvfs[3]) & (df_prof["cl"] == baseline_dvfs[4])]
     baseline_troughput, baseline_power = round(baseline["throughput"].iloc[0]), round(baseline["power"].iloc[0])
     chosen_dvfs['1'] = list(baseline_dvfs)
-    moderate = df_prof[(df_prof["throughput"] >= (baseline_troughput * SpeedUp_PowerUp['1'][0])) & (df_prof["power"] <= (baseline_power * SpeedUp_PowerUp['1'][1]))]
+    moderate = df_prof[(df_prof["throughput"] >= (baseline_troughput * SpeedUp_PowerUp['2'][0])) & (df_prof["power"] <= (baseline_power * SpeedUp_PowerUp['2'][1]))]
     if not moderate.empty:
         moderate_dvfs = moderate["cpu_cores"].min(), moderate["cpu_freq"].min(), moderate["gpu_freq"].min(), moderate["memory_freq"].min(), moderate["cl"].min()
         chosen_dvfs['2'] = list(moderate_dvfs)
-    high = df_prof[(df_prof["throughput"] >= (baseline_troughput * SpeedUp_PowerUp['2'][0])) & (df_prof["power"] <= (baseline_power * SpeedUp_PowerUp['2'][1]))]
+    high = df_prof[(df_prof["throughput"] >= (baseline_troughput * SpeedUp_PowerUp['3'][0])) & (df_prof["power"] <= (baseline_power * SpeedUp_PowerUp['3'][1]))]
     if not high.empty:
         high_dvfs = high["cpu_cores"].min(), high["cpu_freq"].min(), high["gpu_freq"].min(), high["memory_freq"].min(), high["cl"].min()
         chosen_dvfs['3'] = list(high_dvfs)
