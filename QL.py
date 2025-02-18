348d328744353cda518c63d5947bf110542ba48a
Ahmad Naufal Labiib Nabhaan <ahmad.nabhaan@students.amikom.ac.id>
2025-02-19 05:31:32 +0700

    update

--- QL.py
+++ QL.py
@@ -381,7 +381,7 @@ for episode in range(num_episodes):
                 np.append(sampled_configs['memory_freq'], np.array(memory_freq))
                 MEM_ACTIONS.append(MEM_ACTIONS[-1]+1)
             if sys.argv[5] == 'jxavier':
-                actions = [(CORES_ACTIONS[-1],), (CPU_ACTIONS[-1],), (GPU_ACTIONS[-1],), (MEM_ACTIONS[-1],), (CL_RANGE.index(cl),)]
+                actions = (CORES_ACTIONS[-1], CPU_ACTIONS[-1], GPU_ACTIONS[-1], MEM_ACTIONS[-1], CL_RANGE.index(cl))
                 action_shape = [len(CORES_ACTIONS), len(CPU_ACTIONS), len(GPU_ACTIONS), len(MEM_ACTIONS), len(CL_ACTIONS)]
                 ranges = [
                     (min(CORES_ACTIONS), max(CORES_ACTIONS) + 1),
@@ -391,7 +391,7 @@ for episode in range(num_episodes):
                     (min(CL_ACTIONS), max(CL_ACTIONS) + 1)
                 ]
             elif sys.argv[5] == 'jorin-nano':
-                actions = [(0,), (CPU_ACTIONS[-1],), (GPU_ACTIONS[-1],), (MEM_ACTIONS[-1],), (CL_RANGE.index(cl),)]
+                actions = (0, CPU_ACTIONS[-1], GPU_ACTIONS[-1], MEM_ACTIONS[-1], CL_RANGE.index(cl))
                 action_shape = [1, len(CPU_ACTIONS), len(GPU_ACTIONS), len(MEM_ACTIONS), len(CL_ACTIONS)]
                 ranges = [
                     (0, 1),
@@ -413,9 +413,9 @@ for episode in range(num_episodes):
         cpu_cores, cpu_freq, gpu_freq, memory_freq, cl = get_best_configuration()
         phase = "post-training"
         if sys.argv[5] == 'jxavier':
-            actions = [(int(np.where(np.atleast_1d(sampled_configs['cpu_cores']) == cpu_cores)[0][0]),), (int(np.where(np.atleast_1d(sampled_configs['cpu_freq']) == cpu_freq)[0][0]),), (int(np.where(np.atleast_1d(sampled_configs['gpu_freq']) == gpu_freq)[0][0]),), (int(np.where(np.atleast_1d(sampled_configs['memory_freq']) == memory_freq)[0][0]),), (CL_RANGE.index(cl),)]
+            actions = (int(np.where(np.atleast_1d(sampled_configs['cpu_cores']) == cpu_cores)[0][0]), int(np.where(np.atleast_1d(sampled_configs['cpu_freq']) == cpu_freq)[0][0]), int(np.where(np.atleast_1d(sampled_configs['gpu_freq']) == gpu_freq)[0][0]), int(np.where(np.atleast_1d(sampled_configs['memory_freq']) == memory_freq)[0][0]), CL_RANGE.index(cl))
         elif sys.argv[5] == 'jorin-nano':
-            actions = [(0,), (int(np.where(np.atleast_1d(sampled_configs['cpu_freq']) == cpu_freq)[0][0]),), (int(np.where(np.atleast_1d(sampled_configs['gpu_freq']) == gpu_freq)[0][0]),), (int(np.where(np.atleast_1d(sampled_configs['memory_freq']) == memory_freq)[0][0]),), (CL_RANGE.index(cl),)]
+            actions = (0, int(np.where(np.atleast_1d(sampled_configs['cpu_freq']) == cpu_freq)[0][0]), int(np.where(np.atleast_1d(sampled_configs['gpu_freq']) == gpu_freq)[0][0]), int(np.where(np.atleast_1d(sampled_configs['memory_freq']) == memory_freq)[0][0]), CL_RANGE.index(cl))
 
     # Print the chosen configuration for tracking
     print({"cpu_cores": cpu_cores+1, "cpu_freq": cpu_freq, "gpu_freq": gpu_freq, "memory_freq": memory_freq, "cl": cl})
