422509288f81d8dc766bf516d8b1f8720c670ebd
Ahmad Naufal Labiib Nabhaan <ahmad.nabhaan@students.amikom.ac.id>
2025-02-19 07:38:39 +0700

    update sample

--- sarsa.py
+++ sarsa.py
@@ -370,16 +370,16 @@ for episode in range(num_episodes):
             cpu_cores, cpu_freq, gpu_freq, memory_freq, cl = generate_neighbor(best_config, second_config)
             if sys.argv[5] == 'jxavier':
                 if cpu_cores not in sampled_configs['cpu_cores']:
-                    np.append(sampled_configs['cpu_cores'], cpu_cores)
+                    sampled_configs['cpu_cores'] = np.append(sampled_configs['cpu_cores'], cpu_cores)
                     CORES_ACTIONS.append(CORES_ACTIONS[-1]+1)
             if cpu_freq not in sampled_configs['cpu_freq']:
-                np.append(sampled_configs['cpu_freq'], cpu_freq)
+                sampled_configs['cpu_freq'] = np.append(sampled_configs['cpu_freq'], cpu_freq)
                 CPU_ACTIONS.append(CPU_ACTIONS[-1]+1)
             elif gpu_freq not in sampled_configs['gpu_freq']:
-                np.append(sampled_configs['gpu_freq'], gpu_freq)
+                sampled_configs['gpu_freq'] = np.append(sampled_configs['gpu_freq'], gpu_freq)
                 GPU_ACTIONS.append(GPU_ACTIONS[-1]+1)
             elif memory_freq not in sampled_configs['memory_freq']:
-                np.append(sampled_configs['memory_freq'], memory_freq)
+                sampled_configs['memory_freq'] = np.append(sampled_configs['memory_freq'], memory_freq)
                 MEM_ACTIONS.append(MEM_ACTIONS[-1]+1)
             if sys.argv[5] == 'jxavier':
                 actions = (CORES_ACTIONS[-1], CPU_ACTIONS[-1], GPU_ACTIONS[-1], MEM_ACTIONS[-1], CL_RANGE.index(cl))
